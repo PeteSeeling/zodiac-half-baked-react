88fcf48013f0dce6ca697e12873d339e2d580c1e
Pete Seeling <peteseeling@gmail.com>
2022-01-24 16:19:59 -0600

    name and year appearing.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css'
 {/* import the zodiac array */}
+import Header from './Header/Header'
+import Main from './Main/Main'
+import Footer from './Footer/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      {/* import and use the Header. Main, and Footer components here */}
+      <Header name = 'Pete' />{/* import and use the Header. Main, and Footer components here */}
       {/* the Header component needs a string passed in as the `name` prop */}
-      {/* the Footer component needs a year passed in as the `year` prop */}
+      <Footer year = '2022' />{/* the Footer component needs a year passed in as the `year` prop */}
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
     </div>
   )
